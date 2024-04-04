61e1650b9dbecb21b554189742a45530242849e9
Johan Thureson <johan.thureson@gmail.com>
2024-04-04 09:23:09 +0200

    Needed NavigationView in ContentView for it to scroll

--- appleTvVideoPlayer/ContentView.swift
+++ appleTvVideoPlayer/ContentView.swift
@@ -9,20 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        TabView {
-            DailyMotionView()
-                .tabItem {
-                    Image(systemName: "1.square.fill")
-                    Text("Tab 1")
-                }
-            Text("Tab 2")
-                .tabItem {
-                    Image(systemName: "2.square.fill")
-                    Text("Tab 2")
-                }
+        NavigationView {
+            TabView {
+                DailyMotionView()
+                    .tabItem {
+                        Image(systemName: "1.square.fill")
+                        Text("Tab 1")
+                    }
+                Text("Tab 2")
+                    .tabItem {
+                        Image(systemName: "2.square.fill")
+                        Text("Tab 2")
+                    }
+            }
         }
-        
     }
 }
 
