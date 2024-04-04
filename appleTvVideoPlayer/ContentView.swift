b9b10c1f2db1fa1ab80ce9c61af12d3c595f4c0d
Johan Thureson <johan.thureson@gmail.com>
2024-04-04 08:40:32 +0200

    Initial Commit

---
+++ appleTvVideoPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  appleTvVideoPlayer
+//
+//  Created by Johan Thureson on 2024-04-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ appleTvVideoPlayer/appleTvVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleTvVideoPlayerApp.swift
+//  appleTvVideoPlayer
+//
+//  Created by Johan Thureson on 2024-04-04.
+//
+
+import SwiftUI
+
+@main
+struct appleTvVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
